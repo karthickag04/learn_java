fcc9c1d2d3f6962e34627d96d65591532736b4d3
karthickag04 <karthickag04@gmail.com>
2025-04-18 11:22:30 +0530

    added array with for example

---
+++ session_00/JavaArrayWithFOR.java
@@ -0,0 +1,55 @@
+public class JavaArrayWithFOR {
+    public static void main(String[] args) {
+
+        String[] flavors = {"Chocolate", "Vanilla", "Strawberry", "Mango", "Pista"};
+for (int i = 0; i < flavors.length; i++) {
+    System.out.println("Yummy flavor #" + (i + 1) + ": " + flavors[i]);
+}
+
+String[] animals = {"Tiger", "Elephant", "Kangaroo", "Zebra", "Panda"};
+for (int i = 0; i < animals.length; i++) {
+    System.out.println("I love the " + animals[i] + " 🐾");
+}
+
+
+String[] heroes = {"Spider-Man", "Iron Man", "Captain Marvel", "Hulk", "Batman"};
+for (int i = 0; i < heroes.length; i++) {
+    System.out.println(heroes[i] + " saves the day! 🦸‍♂️");
+}
+
+
+String[] cartoons = {"Mickey", "Tom", "Jerry", "Doraemon", "SpongeBob"};
+for (int i = 0; i < cartoons.length; i++) {
+    System.out.println("I watch " + cartoons[i] + " every weekend!");
+}
+
+
+String[] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+for (int i = 0; i < planets.length; i++) {
+    System.out.println("Planet #" + (i + 1) + " is " + planets[i]);
+}
+
+
+String[] foods = {"Pizza", "Burger", "French Fries", "Noodles", "Ice Cream"};
+for (int i = 0; i < foods.length; i++) {
+    System.out.println("I want to eat: " + foods[i]);
+}
+String[] instruments = {"Guitar", "Piano", "Drums", "Flute", "Violin"};
+for (int i = 0; i < instruments.length; i++) {
+    System.out.println("Let's play the " + instruments[i]);
+}
+String[] gameCharacters = {"Mario", "Luigi", "Sonic", "Pikachu", "Link"};
+for (int i = 0; i < gameCharacters.length; i++) {
+    System.out.println(gameCharacters[i] + " is ready to play!");
+}
+String[] sports = {"Cricket", "Football", "Basketball", "Tennis", "Hockey"};
+for (int i = 0; i < sports.length; i++) {
+    System.out.println("Let's play " + sports[i] + " today!");
+}
+String[] partyItems = {"Cake", "Balloons", "Gifts", "Party Hats", "Music"};
+for (int i = 0; i < partyItems.length; i++) {
+    System.out.println("Don't forget the " + partyItems[i] + "!");
+}
+
+
+    }}
