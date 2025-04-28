c167806000fadc0e655b21564d55992c3f03b3e9
karthickag04 <karthickag04@gmail.com>
2025-04-28 12:33:07 +0530

    added session file on void method

---
+++ session_00a/JavaProgram08Std.java
@@ -0,0 +1,32 @@
+public class JavaProgram08Std {
+    public static void main(String[] args){
+        class8A();
+        class8A(1);
+        class8A("kathick",22,212.3d,"new lab");
+
+
+    }
+
+
+    public static void class8A(){
+        System.out.println("8th standard class A");
+    }
+
+    public static void class8A(int x){
+        int result = 1 + 1;
+        System.out.println("8th standard class A "  + result);
+       
+    }
+
+    public static void class8A( String addStaff, int age, double  xxx, String Labname){
+        System.out.println("8th standard class A" + addStaff + " " + age + " " + xxx + " " + Labname);
+    }
+
+    public static void class8B(){
+        System.out.println("8th standard class A");
+    }
+
+
+
+
+}
