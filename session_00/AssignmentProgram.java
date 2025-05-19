0e131b8f62130ee718149bbf47b580078e9788d0
karthickag04 <karthickag04@gmail.com>
2025-05-19 12:16:18 +0530

    added session files on operators

---
+++ session_00/AssignmentProgram.java
@@ -0,0 +1,22 @@
+public class AssignmentProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Assignment Operators
+        a += b; // a = a + b
+        System.out.println("Addition Assignment: " + a); // 30
+
+        a -= b; // a = a - b
+        System.out.println("Subtraction Assignment: " + a); // 10
+
+        a *= b; // a = a * b
+        System.out.println("Multiplication Assignment: " + a); // 200
+
+        a /= b; // a = a / b
+        System.out.println("Division Assignment: " + a); // 10
+
+        a %= b; // a = a % b
+        System.out.println("Modulus Assignment: " + a); // 10
+    }
+}
