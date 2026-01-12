76885e486fabd3249e0abdea53e1e68300c55f85
karthickag04 <karthickag04@gmail.com>
2026-01-12 17:53:54 +0530

    updated syntax files

---
+++ 01-fundamentals/01-syntax/FirstProgram.java
@@ -0,0 +1,30 @@
+public class FirstProgram{ // PascalCase
+
+    public static void main(String [] args){
+
+        int firstPerson = 22;
+        int secondPerson = 23;
+
+
+        // method calling
+        printMessage();   
+        /*
+        fsdafds
+        fsdfsd
+        fsdfsd
+        fsdf
+        dsfsd
+        
+        */
+
+    }
+
+
+    // method declaration
+    public static void printMessage(){ // camelCase
+        System.out.println("Welcome to Java");
+    }
+
+
+}
+
