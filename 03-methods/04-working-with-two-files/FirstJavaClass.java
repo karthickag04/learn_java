9f1cf61919a9d760c510c4fc697e132bf378f255
karthickag04 <karthickag04@gmail.com>
2026-01-21 17:32:51 +0530

    added use case of another file's class inside one more file's class with object and without

---
+++ 03-methods/04-working-with-two-files/FirstJavaClass.java
@@ -0,0 +1,11 @@
+
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        SecondJavaClass sjc=new SecondJavaClass();
+        sjc.SecondClassMethod02();
+        
+        System.out.println("***************");
+        SecondJavaClass.SecondClassMethod01();
+    }
+    
+}
---
+++ 03-methods/04-working-with-two-files/SecondJavaClass.java
@@ -0,0 +1,13 @@
+
+
+public class SecondJavaClass {
+
+    public static void SecondClassMethod01(){
+        System.out.println("Welcome to method 01 under second class with static");
+    }
+
+    public void SecondClassMethod02(){
+        System.out.println("Welcome to method 01 under second class without static");
+    }
+
+}
