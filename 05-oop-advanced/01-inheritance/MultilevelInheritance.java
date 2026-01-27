547882325d7555bec605252cf469ecdd2e7cda25
karthickag04 <karthickag04@gmail.com>
2026-01-27 17:57:24 +0530

    updated multilevel inheritance

--- 05-oop-advanced/01-inheritance/MultilevelInheritance.java
+++ 05-oop-advanced/01-inheritance/MultilevelInheritance.java
@@ -1,28 +1,47 @@
-public class MultilevelInheritance {
-    public static void main(String[] args) {
-        System.out.println("Inheritance Example");
-        System.out.println("************************");
-        People p=new People();
-        p.Admission();
 
-    }
-}
+class College {
 
-
-
-class College{
-
-    public void Admission(){
+    public void Admission() {
         System.out.println("Admission Open");
     }
 
+    public void Facilities() {
+        System.out.println("Facilities");
+    }
+
 }
 
-class People extends College{
+class People extends College {
 
-    public void Admission(){
+    public void Admission() {
         super.Admission();
         System.out.println("Admitted in college");
     }
 
+    public void AdminssionProcess() {
+        System.out.println("Admission Process");
+    }
+
+}
+
+class Student extends People {
+
+    public void Admission() {
+        super.Admission();
+        System.out.println("Admitted in college");
+    }
+
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        // System.out.println("Inheritance Example");
+        // System.out.println("************************");
+        // People p = new People();
+        // p.Admission();
+        Student s = new Student();
+        s.Admission();
+        s.Facilities();
+
+    }
 }
